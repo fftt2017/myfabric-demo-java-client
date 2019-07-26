852f476936140481eaaadb56e284fbe966695b3a
fftt2017 <34053412+fftt2017@users.noreply.github.com>
2019-07-26 10:06:23 +0800

    Update certificate.bat

--- certificate.bat
+++ certificate.bat
@@ -1,14 +1,15 @@
-cd C:\github\scratch\hyperledger\fabric-sdk-java-demo
+set chain_dir=<myfabric-demo-chain dir path>
+set java_client_dir=<myfabric-demo-java-client dir path>
 
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org0\data\org0-ca-chain.pem certificate\org0-ca-chain.pem
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\org1-ca-chain.pem certificate\org1-ca-chain.pem
+copy /y %chain_dir%\org0\data\org0-ca-chain.pem %java_client_dir%\certificate\org0-ca-chain.pem
+copy /y %chain_dir%\org1\data\org1-ca-chain.pem %java_client_dir%\certificate\org1-ca-chain.pem
 
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\admin\msp\keystore\* certificate\admin_sk
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\admin\msp\signcerts\* certificate\admin_cert.pem
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\admin\tls\client.crt certificate\admin_client.crt
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\admin\tls\client.key certificate\admin_client.key
+rem copy /y %chain_dir%\org1\data\admin\msp\keystore\* %java_client_dir%\certificate\admin_sk
+rem copy /y %chain_dir%\org1\data\admin\msp\signcerts\* %java_client_dir%\certificate\admin_cert.pem
+rem copy /y %chain_dir%\org1\data\admin\tls\client.crt %java_client_dir%\certificate\admin_client.crt
+rem copy /y %chain_dir%\org1\data\admin\tls\client.key %java_client_dir%\certificate\admin_client.key
 
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\user\msp\keystore\* certificate\user_sk
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\user\msp\signcerts\* certificate\user_cert.pem
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\user\tls\client.crt certificate\user_client.crt
-copy /y C:\vb_ubuntu\myshare\myfabric-demo\org1\data\user\tls\client.key certificate\user_client.key
+copy /y %chain_dir%\org1\data\user\msp\keystore\* %java_client_dir%\certificate\user_sk
+copy /y %chain_dir%\org1\data\user\msp\signcerts\* %java_client_dir%\certificate\user_cert.pem
+copy /y %chain_dir%\org1\data\user\tls\client.crt %java_client_dir%\certificate\user_client.crt
+copy /y %chain_dir%\org1\data\user\tls\client.key %java_client_dir%\certificate\user_client.key
